c739ac50f9bf8b7dc601ac1c11239a41dbfdfcb7
lani556 <168364782+lani556@users.noreply.github.com>
2026-09-14 16:20:16 -0400

    Product + Customer Class
    Bones of Product and Customer Class
    Have not done edge cases

--- customer.py
+++ customer.py
@@ -1,1 +1,18 @@
 # Customer Module
+
+from cart import Cart
+
+class Customer:
+    def __init__(self, customer_id, name, shoppingcart):
+        self.customer_id = customer_id
+        self.name = name
+        self.cart = Cart(shoppingcart)
+
+    def get_id(self):
+        return f"{self.customer_id}"
+
+    def get_name(self):
+        return f"{self.name}" 
+
+    def get_cart(self):
+        return self.cart
--- product.py
+++ product.py
@@ -1,1 +1,22 @@
 #Product Module
+
+class Product:
+    def __init__(self, product_id, name, price):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+
+    def get_id(self):
+        return f"{self.product_id}"
+
+    def get_name(self):
+        return f"{self.name}"
+
+    def get_price(self):
+        return f"{self.price}"
+
+pr1 = Product(1, "Product 1", 10.99)
+
+print(pr1.get_id())
+print(pr1.get_name())
+print(pr1.get_price())
